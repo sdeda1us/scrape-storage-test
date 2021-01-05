aba54515f25af9dab67d4ee788d0659f615b9f90
sdeda1us <steven.amloney@gmail.com>
2021-01-05 09:01:37 -0600

    cheerio is pulling down site

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import './App.css';
 import styled, {css} from 'styled-components';
 
 
 
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {dispatch({type:'SCRAPE_PLAY'})}, []);
+
+  const playData = useSelector(state => state.playReducer);
+
   return (
     <div className="App">
       <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -9,22 +9,50 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import {takeEvery, put, call} from 'redux-saga/effects';
 import Axios from 'axios';
+import cheerio from 'cheerio';
 
 
 const sagaMiddleware = createSagaMiddleware();
 
 //----------------------WATCHER SAGA------------------------------//
 function* rootSaga() {
-
+    yield takeEvery('SCRAPE_PLAY', scrapePlay);
 }
 
-const templateReducer = (state=[], action) => {
+function* scrapePlay() {
+    try {
+        const response = yield call(Axios.get, `https://cors-anywhere.herokuapp.com/http://shakespeare.mit.edu/henryv/full.html`);
+        yield put({type: 'SET_PLAY', payload: response.data});
+    } catch (error) {
+        console.log('error fetching play', error);
+    }
+}
+
+const playReducer = (state=[], action) => {
+    if (action.type === 'SET_PLAY'){
+        let locations = [];
+        let lines =[];
+        let $ = cheerio.load(action.payload);
+        $('h3').each(function(i, element) {
+            let location = $(this)
+                .prepend()
+                .text();
+            locations.push(location);
+        });
+        $('a').each(function(i, element) {
+            let line= $(this)
+                .prepend()
+                .text();
+            lines.push(line);
+        })
+        return [locations, lines];
+    }
     return state;
 }
 
 const storeInstance = createStore( 
     (combineReducers({
-        templateReducer
+        playReducer
     })),
         applyMiddleware(sagaMiddleware, logger)
     );
